19aa743f3d177e47105d568ba8a17ac51eb53e3a
rupali-ht <rupalit@heaptrace.com>
2021-05-07 00:59:29 +0530

    new file created

--- Demo.java
+++ Demo.java
@@ -2,7 +2,9 @@ package com_practice;
 
 public class Demo {
 	public static void main(String asr[]) {
-		System.out.println("Create account on Github"+"\n"+"I am creating new git Repository"+"\n"+"Used Git Bash");
+		System.out.println("Create account on Github"+"\n"+"I am creating new git Repository"+"\n"+"Used Git Bash +"\n"+"
+Create account on Github"+"\n"+"I am creating new git Repository"+"\n"+"Used Git Bash "
+);
 	}
 
 }
